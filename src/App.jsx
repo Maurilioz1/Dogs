9c2712cb8e6fb36ae5ca9a1b842307a5a17ec023
Maurilio <maurilioz1@hotmail.com>
2024-05-15 07:46:11 -0300

    userContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Login from './Components/Login/Login';
 import './App.css';
+import { UserStorage } from './Contexts/UserContext';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-      <Footer />
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </UserStorage>
     </BrowserRouter>
   );
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,27 @@ import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import Dogs from '../../Assets/dogs.svg?react';
+import { UserContext } from '../../Contexts/UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" className={styles.logo} aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login" className={styles.login}>
-          Login / Criar
-        </Link>
+
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
