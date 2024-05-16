5632f661a9a0b63260890b67db8bc01448dc6da1
Maurilio <maurilioz1@hotmail.com>
2024-05-16 12:58:42 -0300

    login create

--- src/Components/Forms/Input/Input.jsx
+++ src/Components/Forms/Input/Input.jsx
@@ -4,9 +4,12 @@ import styles from './Input.module.css';
 const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
-      <label htmlFor={name} className={styles.label}>
-        {label}
-      </label>
+      {label && (
+        <label htmlFor={name} className={styles.label}>
+          {label}
+        </label>
+      )}
+
       <input
         id={name}
         name={name}
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'E-mail inválido.',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+    message:
+      'A senha deve conter pelo menos um número, uma letra minúscula, uma letra maiúscula e no mínimo 8 caracteres.',
+  },
 };
 
 const useForm = (type) => {
