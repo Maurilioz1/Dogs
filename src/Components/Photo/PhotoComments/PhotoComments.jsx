1fffd771344a0c07bf76807aae7949a14e94e020
Maurilio <maurilioz1@hotmail.com>
2024-05-23 10:10:46 -0300

    post comment

--- src/Components/Photo/PhotoComments/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments/PhotoComments.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import styles from './PhotoComments.module.css';
+import { UserContext } from '../../../Contexts/UserContext';
+import PhotoCommentsForm from '../PhotoCommentsForm/PhotoCommentsForm';
 
-const PhotoComments = () => {
-  return <div className={styles.comments}>PhotoComments</div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
