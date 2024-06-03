d4dd558336b3a8c94ca3456ccccf186625d5e209
Maurilio <maurilioz1@hotmail.com>
2024-06-03 18:47:17 -0300

    graph

--- src/Components/User/UserStatsGraphs/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs/UserStatsGraphs.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
 import styles from './UserStatsGraphs.module.css';
+import { VictoryBar, VictoryChart, VictoryPie } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
   const [graph, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
+    const graphData = data.map((item) => ({ x: item.title, y: Number(item.acessos) }));
+
     setTotal(data.map(({ acessos }) => Number(acessos)).reduce((prev, next) => prev + next, 0));
+    setGraph(graphData);
   }, [data]);
 
   return (
     <section className={`${styles.graph} anime-left`}>
-      <div className={styles.total}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Acessos: {total}</p>
+      </div>
+
+      <div className={styles.graphItem}>
+        <VictoryPie
+          data={graph}
+          innerRadius={50}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#ffffff',
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333333',
+            },
+          }}
+        />
+      </div>
+
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar data={graph} alignment="start" />
+        </VictoryChart>
       </div>
     </section>
   );
